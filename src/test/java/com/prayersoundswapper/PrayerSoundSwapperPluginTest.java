349f01f0a2ca33e5528bd1774ab53ef851502728
Aaron Enberg <aaronenberg@microsoft.com>
2026-06-25 21:40:07 -0700

    add prayer sound swapping

--- src/test/java/com/prayersoundswapper/PrayerSoundSwapperPluginTest.java
+++ src/test/java/com/prayersoundswapper/PrayerSoundSwapperPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.prayersoundswapper;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PrayerSoundSwapperPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PrayerSoundSwapperPlugin.class);
 		RuneLite.main(args);
 	}
 }
